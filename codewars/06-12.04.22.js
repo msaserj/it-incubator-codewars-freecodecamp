d40c68e8e331eac23a478b66526ffabc8f7563a0
msaserj <serg.ks@gmail.com>
2022-04-12 22:26:18 +0300

    codewars 4 tasks

---
+++ codewars/06-12.04.22.js
@@ -0,0 +1,10 @@
+//Square Every Digit
+
+function squareDigits(num){
+    let number = '' + num;
+    let newNum = new Array();
+    number.split('').map(n => {
+        newNum.push(n**2);
+    })
+    return parseInt(newNum.join(''));
+}
